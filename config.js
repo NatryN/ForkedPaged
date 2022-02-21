235e717ef2b9de5c52fd0401cbc9384ffe1c026c
Natry Nevnf <66025265+P4NAD3ROXIS@users.noreply.github.com>
2022-02-20 19:43:41 -0500

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ const CONFIG = {
   greetingNight: 'Qué haces despierto',
 
   // Layout
-  bentoLayout: 'lists', // 'bento', 'lists', 'buttons'
+  bentoLayout: 'buttons', // 'bento', 'lists', 'buttons'
 
   // Weather
   weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
